7ead004f4f73cfbe99b908405065262ba476e9b0
Mehmet Ziya <mehmetziya82@gmail.com>
2021-11-08 13:08:03 +0100

    morgan

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import connectDB from './config/connectDB.js'
 
@@ -17,14 +18,27 @@ app.use(express.json())
 app.use('/api/users', userRoutes)
 
 //Middleware
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 4000
 
-app.listen(
-  PORT,
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err)
+  }
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-)
+})
